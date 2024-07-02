f34e95b8d6e737b77c296d8514b46f6fedb54162
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2024-07-02 08:19:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa- alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = 
+
+    {
+    enunciado: "Qual pais tem o maior indice de pobreza?",
+    alternativas: ["Africa", "Somália"],
+    },
+    
+    {
+    enunciado: “Pergunta 2”,
+    alternativas: [“Alternativa 1”, “Alternativa 2”],
+    },
