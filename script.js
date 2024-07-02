d473f1911cacfd0b3b5949ee7aa1e24c7a07f4c3
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2024-07-02 09:53:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,43 @@ const caixaAlternativas = document.querySelector(".caixa- alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = 
-
+const perguntas = [
     {
     enunciado: "Qual pais tem o maior indice de pobreza?",
-    alternativas: ["Africa", "Somália"],
-    },
+    alternativas: [
+
+    {texto: "África",
+     afirmação: afirmação da alternativa 1"
+     },
+
+     {texto: "Somália",
+     afirmação: afirmação da alternativa 1"
+     },
+
+     {texto: "Cuba",
+     afirmação: afirmação da alternativa 1"
+     },
     
-    {
-    enunciado: “Pergunta 2”,
-    alternativas: [“Alternativa 1”, “Alternativa 2”],
-    },
+    ]
+
+       
+        }
+        "Africa",
+         "Somália", 
+         "Cuba",
+        },
+        
+        {
+        enunciado: "Qual o estado brasileiro mais racista?",
+        alternativas: ["São Paulo", "Paraná", "Rio Grande do Sul"],
+        },
+        
+        {
+        enunciado: "Qual dos seguintes fatores é mais frequentemente citado como uma causa primária da desigualdade social em sociedades modernas?",
+        alternativas: ["Distribuição desigual de ", ""],
+        },
+    
+
+]
+
+   
